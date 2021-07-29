206f6a83d54766ddb43aefcb86898c8133971a3d
Nilkumar Patel <nsp4898@gmail.com>
2021-07-29 18:08:40 -0400

    Completed all the Todos

--- CP380-PubsLab/Program.cs
+++ CP380-PubsLab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CP380_PubsLab
 {
@@ -13,28 +14,55 @@ namespace CP380_PubsLab
                     Console.WriteLine("Yes, I can connect");
                 }
 
-                // 1:Many practice
-                //
-                // TODO: - Loop through each employee
-                //       - For each employee, list their job description (job_desc, in the jobs table)
+                // 1:Many
+                var employees = dbcontext.Employee.ToList();
+                var jobs = dbcontext.Jobs.ToList();
 
-                // TODO: - Loop through all of the jobs
-                //       - For each job, list the employees (first name, last name) that have that job
+                Console.WriteLine("Employee List");
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine("\t> " + employee.fname + " " + employee.lname + " (" + dbcontext.Jobs.First(j => j.job_id == employee.job_id).job_desc + ")");
+                }
 
+                Console.WriteLine("\n\nJob List");
+                foreach (var job in jobs)
+                {
+                    Console.WriteLine("\t" + job.job_desc);
+                    var tempEmployees = dbcontext.Employee.Where(e => e.job_id == job.job_id).ToList();
+                    foreach (var employee in tempEmployees)
+                    {
+                        Console.WriteLine("\t\t" + employee.fname + " " + employee.lname);
+                    }
+                }
 
-                // Many:many practice
-                //
-                // TODO: - Loop through each Store
-                //       - For each store, list all the titles sold at that store
-                //
-                // e.g.
-                //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
-                
-                // TODO: - Loop through each Title
-                //       - For each title, list all the stores it was sold at
-                //
-                // e.g.
-                //  The Gourmet Microwave -> Doc-U-Mat: Quality Laundry and Books, Bookbeat
+                // Many:Many
+                var stores = dbcontext.Stores.ToList();
+                var titles = dbcontext.Titles.ToList();
+                var sales = dbcontext.Sales.ToList();
+
+                Console.WriteLine("\n\nStores");
+                foreach (var store in stores)
+                {
+                    Console.Write("\t" + store.stor_name + " => ");
+                    var tempSales = sales.Where(s => s.stor_id == store.stor_id).ToList();
+                    foreach(var sale in tempSales)
+                    {
+                        Console.Write(titles.First(t => t.title_id == sale.title_id).title + ", ");
+                    }
+                    Console.WriteLine("\n");
+                }
+
+                Console.WriteLine("\n\nBooks");
+                foreach (var title in titles)
+                {
+                    Console.Write("\t" + title.title + " => ");
+                    var tempSales = sales.Where(s => s.title_id == title.title_id).ToList();
+                    foreach (var sale in tempSales)
+                    {
+                        Console.Write(stores.First(t => t.stor_id == sale.stor_id).stor_name + ", ");
+                    }
+                    Console.WriteLine("\n");
+                }
             }
         }
     }
